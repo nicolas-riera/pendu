7e0da1fb8a9f7eb81d008b7099d6eaab492c1ba8
arthur-georget <arthur.georget@laplateforme.io>
2026-01-23 16:39:29 +0100

    docs(gamedata): add keyboard_input.py functions docstrings

--- src/keyboard_input.py
+++ src/keyboard_input.py
@@ -6,6 +6,19 @@ import unicodedata
 # Functions
 
 def keyboard_input(events):
+
+    '''
+    Translate in a more friendly way keyboard inputs selected pygame events.
+    All events other than backspace, enter and letters are ignored.
+    #### For example: 
+            1. pygame.K_BACKSPACE turns into "backspace".
+            2. pygame.K_RETURN turns into "enter"       
+    ### PARAMETERS
+            events: pygame.Events
+    ### RETURNS
+            str
+    '''
+
     for event in events:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_BACKSPACE:
