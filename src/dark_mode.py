21b4d39b8d16ce401b6874cc1ea1350b6536a4ce
nicolas-riera <nicolas.riera@laplateforme.io>
2026-01-23 12:23:26 +0100

    other: improved file structure

--- src/dark_mode.py
+++ src/dark_mode.py
@@ -1,23 +1,8 @@
 # Libraries
 
-import os
-
-# Variables
-
-FILE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../", "assets" , "gamedata" ,"config.txt")
+from src.config import *
 
 # Functions
-
-def read_config_file():
-    with open(FILE_PATH, "r", encoding="utf-8") as f:
-        return [line.rstrip("\n") for line in f]
-    
-def write_lines(lines):
-    with open(FILE_PATH, "w", encoding="utf-8") as f:
-        f.writelines(f"{line}\n" for line in lines)
-
-def default_config():
-    write_lines(["is_dark_mode = False"])
 
 def dark_mode_setting(mode, is_dark_mode):
     lines = read_config_file()
