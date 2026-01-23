37b8bd75cdc9cfe1e4693b52bbdb2193d8a0c9e3
nicolas-riera <nicolas.riera@laplateforme.io>
2026-01-23 11:12:01 +0100

    other: changed function syntax to be more natural

--- src/dark_mode.py
+++ src/dark_mode.py
@@ -1,12 +1,12 @@
 def light_dark_mode(screen, is_dark_mode):
     
-    if not is_dark_mode:
-        screen.fill("white")
-        text_color = (0, 0, 0)
-
-    else:
+    if is_dark_mode:
         screen.fill((0, 0, 0))
         text_color = (255, 255, 255)
+
+    else:
+        screen.fill((255, 255, 255))
+        text_color = (0, 0, 0)
 
     return text_color
 
